b149d288c64586d84fc3c5f5f2491a191214bd40
Sandeep Rishi Tadi <123352611+18Sandeep18@users.noreply.github.com>
2025-05-16 20:19:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://fresh-meat-hub.vercel.app/',
   methods: ['GET', 'POST'],
   credentials: true,
 }));
